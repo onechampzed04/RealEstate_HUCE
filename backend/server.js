6169d4f1fd0dbab81d9db47a8663a26a83e1ce74
Phuong285618 <evo561218@gmail.com>
2026-02-21 18:10:16 +0700

    test

--- backend/server.js
+++ backend/server.js
@@ -29,4 +29,6 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => 
+  console.log(`Server running on port ${PORT}`)
+);
